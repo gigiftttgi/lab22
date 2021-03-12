51fff9557814925a01921b758b8fe2fff293a1f8
gigiftttgi <76035585+gigiftttgi@users.noreply.github.com>
2021-03-12 16:21:58 +0700

    Update lab22_3.cpp

--- lab22_3.cpp
+++ lab22_3.cpp
@@ -2,7 +2,15 @@
 
 using namespace std;
 
-//Write function count() here.
+int count(int a[], int s){
+	set<int> new_a;
+
+	for(int i = 0; i<s; i++){
+		new_a.insert(a[i]);
+	}
+
+	return new_a.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
